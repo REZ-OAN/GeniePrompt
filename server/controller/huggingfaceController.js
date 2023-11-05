1093d6925ea0180efc271542917fcfb797827d60
REZ-OAN <ahmedabir.rez@gmail.com>
2023-11-05 11:32:25 +0600

    made changes on huggingfaceController.js

--- server/controller/huggingfaceController.js
+++ server/controller/huggingfaceController.js
@@ -36,10 +36,10 @@ export const promptController = async (req, res) => {
         const { text } = req.body;
         const generator = await new Pipeline(
             "text2text-generation",
-            "Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum"
+            "merve/chatgpt-prompts-bart"
         );
         const response = await fetch(
-            "https://api-inference.huggingface.co/models/Kaludi/chatgpt-gpt4-prompts-bart-large-cnn-samsum",
+            "https://api-inference.huggingface.co/models/merve/chatgpt-prompts-bart",
             {
                 headers: {
                     Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
@@ -51,7 +51,11 @@ export const promptController = async (req, res) => {
         const result = await response.json();
 
         if (result) {
-            return res.status(200).json(result[0]["generated_text"]);
+            return res
+                .status(200)
+                .json(
+                    `Include The Generated Prompt to your wants\n${result[0]["generated_text"]}`
+                );
         }
     } catch (err) {
         console.log(err);
