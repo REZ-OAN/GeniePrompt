355086a5dae837d7a6eb2a11599eeda59c642ef5
REZ-OAN <ahmedabir.rez@gmail.com>
2023-11-08 00:52:14 +0600

    added validation in email and username

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,11 +5,20 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: ["true", "username must be given"],
+        minlength: 8,
+        maxlength: 20,
     },
     email: {
         type: String,
         required: [true, "email must be given"],
         unique: true,
+        validate: {
+            validator: function (value) {
+                const pattern =
+                    /^[^._-][a-zA-Z0-9._-]+@(gmail|yahoo|hotmail)\.[a-zA-Z]+/g;
+                return pattern.test(value);
+            },
+        },
     },
     password: {
         type: String,
